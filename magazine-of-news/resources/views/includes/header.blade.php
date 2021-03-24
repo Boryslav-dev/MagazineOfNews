12b3948105a30c5a3279d33579dc61f6ab8c4c3d
Vafflentine <kartoshkapink@gmail.com>
2021-03-24 14:12:27 +0200

    Add NavBar to site navigation

---
+++ magazine-of-news/resources/views/includes/header.blade.php
@@ -0,0 +1,39 @@
+
+<header id="header">
+    <nav class="navbar fixed-top bg-light navbar-expand-lg navbar-light center">
+        <div class="container sticky-top">
+            <a class="navbar-brand center" href="/"><b>Time</b></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Category
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Football</a>
+          <a class="dropdown-item" href="#">Basketball</a>
+          <a class="dropdown-item" href="#">Golf</a>
+          <a class="dropdown-item" href="#">Tennis</a>
+          <a class="dropdown-item" href="#">Voleyball</a>
+          <a class="dropdown-item" href="#">Cricket</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact us</a>
+      </li>
+    </ul>
+  </div>
+        </div>
+    </nav>
+</header>
